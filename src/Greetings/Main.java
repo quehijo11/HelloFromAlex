63e92a5ff0ea60e5f4d677c0bfdaac6c5d17bfc7
Meanravage <adallsup@gmail.com>
2021-10-05 21:45:58 -0600

    Aaron update 10/05/2021

--- src/Greetings/Main.java
+++ src/Greetings/Main.java
@@ -3,7 +3,8 @@ package Greetings;
 public class Main {
     public static void main(String[] args) {
         String alexName = "Alex";
-
+        String aaronName = "Aaron";
         System.out.println("Hello, my name is " + alexName);
+        System.out.println("Hello" + alexName + ",my name is " + aaronName);
     }
 }
