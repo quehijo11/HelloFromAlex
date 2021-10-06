03b1c2a18e79da3ea1dbb36528201f8d0773310d
Quehijo11 <wat15029@byui.edu>
2021-10-05 21:39:58 -0600

    Initial commit

---
+++ src/Greetings/Main.java
@@ -0,0 +1,9 @@
+package Greetings;
+
+public class Main {
+    public static void main(String[] args) {
+        String alexName = "Alex";
+
+        System.out.println("Hello, my name is " + alexName);
+    }
+}
